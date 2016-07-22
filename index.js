e22f85c378247e6aeabcb56a0561c81cea46a44b
Anton <anton@sobesednik.media>
2016-07-22 19:57:13 +0300

    check redirect_uri in did-get-redirect-request function

--- index.js
+++ index.js
@@ -50,11 +50,11 @@ function authenticateVK(options, windowOptions) {
     win.loadURL(vkurl);
 
     return new Promise((resolve, reject) => {
-        win.webContents.on('did-get-redirect-request', function (event, oldUrl, newUrl) {
+        win.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl) => {
             const data = url.parse(newUrl);
             debug(data);
             // http://stackoverflow.com/questions/16733863/oauth2-0-implicit-grant-flow-why-use-url-hash-fragments
-            if (data.host === 'oauth.vk.com' && data.pathname === '/blank.html' && data.hash) {
+            if (`${data.protocol}//${data.host}${data.pathname}` === opts.redirectUri && data.hash) {
                 const query = qs.parse(data.hash.substring(1));
                 debug(query);
 
